ac28340af71e618afbc72ea2a9170dcbf7fdb9d8
d5ng <dong5ffice@gmail.com>
2024-12-14 19:24:16 +0900

    ✨ Feat: counter reducer example add

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,29 +1,58 @@
-interface InitialState {
-  a: number
-  b: number
-}
+/**
+ * Object 사용 예시
+ */
 
-const initialState = {
-  a: 10,
-  b: 20,
-}
+// interface InitialState {
+//   a: number
+//   b: number
+// }
+
+// const initialState = {
+//   a: 10,
+//   b: 20,
+// }
+
+// export const ACTION_TYPE = {
+//   SET_A: 'redux/SET_A',
+//   SET_B: 'redux/SET_B',
+// } as const
+
+// type Action =
+//   | { type: typeof ACTION_TYPE.SET_A; payload: Pick<InitialState, 'a'> }
+//   | { type: typeof ACTION_TYPE.SET_B; payload: Pick<InitialState, 'b'> }
+
+// export const additionReducer = (state: InitialState = initialState, action: Action): InitialState => {
+//   switch (action.type) {
+//     case ACTION_TYPE.SET_A:
+//       return { ...state, a: action.payload.a }
+
+//     case ACTION_TYPE.SET_B:
+//       return { ...state, b: action.payload.b }
+
+//     default:
+//       return state
+//   }
+// }
+
+/**
+ * Number 사용 예시
+ */
+const initialState = 0
 
 export const ACTION_TYPE = {
-  SET_A: 'redux/SET_A',
-  SET_B: 'redux/SET_B',
+  INCREASE: 'increase',
+  DECREASE: 'decrease',
 } as const
 
-type Action =
-  | { type: typeof ACTION_TYPE.SET_A; payload: Pick<InitialState, 'a'> }
-  | { type: typeof ACTION_TYPE.SET_B; payload: Pick<InitialState, 'b'> }
+type Action = { type: typeof ACTION_TYPE.INCREASE } | { type: typeof ACTION_TYPE.DECREASE }
 
-export const additionReducer = (state: InitialState = initialState, action: Action): InitialState => {
+export const additionReducer = (state: number = initialState, action: Action): number => {
   switch (action.type) {
-    case ACTION_TYPE.SET_A:
-      return { ...state, a: action.payload.a }
+    case ACTION_TYPE.INCREASE:
+      return state + 1
 
-    case ACTION_TYPE.SET_B:
-      return { ...state, b: action.payload.b }
+    case ACTION_TYPE.DECREASE:
+      return state <= 0 ? 0 : state - 1
 
     default:
       return state
