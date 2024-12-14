f2996ce23f8fd4f9ae90d7a0ecf60d7cb049707a
d5ng <dong5ffice@gmail.com>
2024-12-14 19:25:03 +0900

    ✨ Feat: Redux Core Exception handling

--- src/core/redux.ts
+++ src/core/redux.ts
@@ -1,3 +1,4 @@
+import isPlainObject from '../utils/isPlainObject'
 import type { Reducer, ActionType, ListenerCallback } from './redux.type'
 import { ActionTypes } from './redux.utils'
 
@@ -6,17 +7,30 @@ export function createStore<State, Action extends ActionType>(reducer: Reducer<S
   let currentListenerId = 0
   let currentListeners: Map<number, ListenerCallback> = new Map()
 
-  const getState = () => ({ ...currentState })
+  const getState = () => currentState
 
   const dispatch = (action: Action) => {
+    if (!isPlainObject(action)) {
+      throw new Error(`해당 ${action}은 순수 객체가 아니에요! action은 항상 순수 객체여야 합니다.`)
+    }
+
+    if (typeof action.type !== 'string') {
+      throw new Error(`action type은 반드시 문자열이어야 합니다.`)
+    }
+
     currentState = reducer(currentState, action)
     currentListeners.forEach((listener) => listener())
-    return action
+
+    return action as Action
   }
 
-  const subscribe = (listeners: ListenerCallback) => {
+  const subscribe = (listenerCallback: ListenerCallback) => {
+    if (typeof listenerCallback !== 'function') {
+      throw new Error(`subscribe 매개변수는 반드시 함수이어야 합니다.`)
+    }
+
     const listenerId = currentListenerId++
-    currentListeners.set(listenerId, listeners)
+    currentListeners.set(listenerId, listenerCallback)
     currentListeners.forEach((listener) => listener())
     return () => {}
   }
